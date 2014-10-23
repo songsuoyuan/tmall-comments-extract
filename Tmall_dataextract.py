5ffe4a48099e90069daf58d35007aaf4164106c1
twofat <zk1212@mail.ustc.edu.cn>
2014-10-23 23:21:34 +0800

    Add a function: Tmall_tagCollect
    Now we can collect a list of tags (including tagID and
    positive/negative) of a given item. Following a specific tagId and
    posi, we can get corresponding reviews (max:2000).

--- Tmall_dataextract.py
+++ Tmall_dataextract.py
@@ -7,12 +7,31 @@ import time
 import sys, os
 import urllib
 
-def Tmall_reviewCollect(itemId,sellerId,data_path='',max_pages=100):
-	filename = data_path + 'Tmall_' + str(itemId) + '_' + str(sellerId) + '.txt'
+
+
+def Tmall_tagCollect(itemId, sellerId, data_path=''):
+	filename = data_path + 'Tag_' + str(itemId) + '_' + str(sellerId) + '.txt'
+	f = open(filename,'w')
+	req = urllib2.Request('http://rate.tmall.com/listTagClouds.htm?itemId='+ str(itemId) +'&isAll=true&isInner=true&t='+str(sellerId)+'&callback=jsonp2003')
+	response = urllib2.urlopen(req)
+	the_page = response.read()
+	f.write(the_page)
+	tag_id = re.findall('{\"count\":[0-9]*?,\"id\":\"([0-9]*?)\",\"posi\":(.*?),\"tag\":\".*?\",\"weight\":[0-9]*?}', the_page)
+	time.sleep(0.2)
+	return(tag_id)
+
+def Tmall_reviewCollect(itemId,sellerId,data_path='',tagId='', posi='', max_pages=100):
+	if tagId == '':
+		filename = data_path + 'Tmall_' + str(itemId) + '_' + str(sellerId) + '.txt'
+	else:
+		filename = data_path + 'Tmall_' + str(itemId) + '_' + str(sellerId) + '_' \
+		+ str(tagId) + _ str(posi) + '.txt'
 	f = open(filename,'w')
 	page = 1
 	while True:
-		req = urllib2.Request('http://rate.tmall.com/list_detail_rate.htm?itemId='+ str(itemId) +'&sellerId='+str(sellerId)+'&order=1&currentPage='+ str(page) +'&callback=jsonp3023')
+		req = urllib2.Request('http://rate.tmall.com/list_detail_rate.htm?itemId='+ \
+			str(itemId) +'&sellerId='+str(sellerId)+'&order=1&currentPage='+ str(page) +\
+			'tagId=' + str(tagId) + 'posi=' + str(posi) + '&callback=jsonp3023')
 		response = urllib2.urlopen(req)
 		the_page = response.read()
 		f.write(the_page)
@@ -57,14 +76,17 @@ def Tmall_searchCollect(search_content,data_path='',max_pages=100):
 def Tmall_searchandreview(search_content,data_path,search_max_pages=100,review_max_pages=100):
 	search_content = search_content.decode('utf-8').encode('gbk')
 	search_content = urllib.quote(search_content)
-	allId = Tmall_searchCollect(search_content, data_path, search_max_pages)
 	data_path = data_path+'Tmall_'+search_content + '/'
 	os.mkdir(data_path)
+	allId = Tmall_searchCollect(search_content, data_path, search_max_pages)
 	for oneId in allId:
-	 	print oneId[0]
-	 	Tmall_reviewCollect(oneId[0], oneId[1], data_path, review_max_pages)
+	  	print oneId[0]
+	  	Tmall_reviewCollect(oneId[0], oneId[1], data_path, review_max_pages)
+
+
+# Tmall_searchandreview('王楷', 'C:/data/', 1)
+
+temp = Tmall_tagCollect(3459421898, 294040860)
 
 
 
-Tmall_searchandreview('移动电源', 'C:/data/', 2)
-	
